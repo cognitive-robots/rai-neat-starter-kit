d81d0e8102d3c4adb9420997a80a5e080c7327cb
Pratik Somaiya <pratik.somaiya2000@gmail.com>
2024-02-05 15:10:34 +0000

    initial working version

--- leaderboard/scripts/run_evaluation.sh
+++ leaderboard/scripts/run_evaluation.sh
@@ -1,6 +1,7 @@
 # CARLA path
-export CARLA_ROOT=/home/kchitta/Documents/CARLA_0.9.10.1 # path to CARLA install
-export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
+
+export CARLA_ROOT=/home/pratik/carla # path to CARLA install
+# export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
 export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
@@ -16,15 +17,17 @@ export PORT=2000 # same as the carla server port
 export TM_PORT=8000 # port for traffic manager, required when spawning multiple servers/clients
 
 # Evaluation Setup
-export ROUTES=leaderboard/data/evaluation_routes/eval_routes_weathers.xml # routes file
+export ROUTES=leaderboard/data/evaluation_routes/eval_routes_weathers.xml # routes file # TODO: getting map not found error for some town
+# export ROUTES=leaderboard/data/training_routes/routes_town01_short.xml
 export SCENARIOS=leaderboard/data/scenarios/eval_scenarios.json # scenarios file
 export REPETITIONS=1 # number of evaluation runs
 export RESUME=0 # resume evaluation from partial results file
 
 # Agent Paths
-export TEAM_AGENT=leaderboard/team_code/auto_pilot.py # agent script
+AGENT=neat_agent
+export TEAM_AGENT=leaderboard/team_code/${AGENT}.py # agent script
 export TEAM_CONFIG=./model_ckpt/neat # model checkpoint (not required for auto_pilot)
-export CHECKPOINT_ENDPOINT=../carla_results/auto_pilot.json # output results file
+export CHECKPOINT_ENDPOINT=./results/${AGENT}.json # output results file
 # export SAVE_PATH=../carla_results/auto_pilot_eval # path for saving episodes (comment to disable)
 
 python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
